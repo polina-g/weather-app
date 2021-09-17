9c1a112571f3b4f4735bcbabf3ffa330e467cde8
polina-g <ignatieva.polina@gmail.com>
2021-09-17 16:20:45 -0400

    create boilerplate outline

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+//GLOBAL VARIABLES AND CONSTANTS
+const API_KEY = 'a0644fddc5d5dbb6f9d0afa73d298677';
+const BASE_URL = 'api.openweathermap.org/data/2.5/weather';
+
+//Cache DOM elements:
+const $city = $('#city')
+const $temperature = $('#temperature')
+const $feelsLike = $('#feels-like')
+const $weather = $('#weather')
+const $input = $('input[type=text]')
+const $getWeather = $('input[type=submit]')
+
+
+
+
+
+
+
+
+});
