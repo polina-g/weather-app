c3e0e7c08e046c30a8f40b47badede1e3bcf3e57
polina-g <ignatieva.polina@gmail.com>
2021-09-17 22:25:28 -0400

    minimal requirement lab complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-$(function() {
+//$(function() {
 //GLOBAL VARIABLES AND CONSTANTS
 const API_KEY = 'a0644fddc5d5dbb6f9d0afa73d298677';
-const BASE_URL = 'api.openweathermap.org/data/2.5/weather';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+let weatherData;
 
 //Cache DOM elements:
 const $city = $('#city')
@@ -10,12 +11,39 @@ const $feelsLike = $('#feels-like')
 const $weather = $('#weather')
 const $input = $('input[type=text]')
 const $getWeather = $('input[type=submit]')
+const $form = $('form');
+
+$form.on('submit', handleWeatherData);
+
+function handleWeatherData (evt) {
+    evt.preventDefault();
+    let cityName = $input.val();
+    $input.val('');
+    $.ajax(`${BASE_URL}weather?q=${cityName}&appid=${API_KEY}`).then(function(data) {
+        weatherData = data;
+        renderTemplate();
+    }, function(error){
+        console.log('error- something fancy later');
+    });
+
+    function renderTemplate() {
+        $city.text(`Weather For: ${weatherData.name}`);
+        $temperature.text(`Temperature: ${kToF(weatherData.main.temp)}`).append('&#176; F');
+        $feelsLike.text(`Feels Like: ${kToF(weatherData.main.feels_like)}`).append('&#176; F');
+        $weather.text(`Weather: ${weatherData.weather[0].description}`)
+
+    }
+
+}
+
+    function kToF(kTemperature) {
+        return Math.round((parseInt(kTemperature) - 273.25) * 9/5 + 32);
+    }
+
+
+    
 
 
 
 
-
-
-
-
-});
+//});
